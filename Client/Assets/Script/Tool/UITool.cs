653cd01afc0a7503b9ca3f4e459cc42b2ac1d83e
水嵐 <joushior@yahoo.com.tw>
2015-03-26 13:57:13 +0800

    UI產生工具

---
+++ Client/Assets/Script/Tool/UITool.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITool : MonoBehaviour {
+
+    public static UITool pthis;
+    void Awake()
+    {
+        pthis = this;
+    }
+
+    public GameObject CreatePanel(string Path)
+    {
+        return NGUITools.AddChild(gameObject, Resources.Load(Path) as GameObject);
+    }
+
+    public GameObject CreateUI(GameObject Parent, string Path)
+    {
+        return NGUITools.AddChild(Parent, Resources.Load(Path) as GameObject);
+    }
+
+    public GameObject CreateUIByPos(GameObject Parent, string Name, float fPosX, float fPosY)
+    {
+        GameObject pObj = NGUITools.AddChild(Parent, Resources.Load("Prefab/" + Name) as GameObject);
+        pObj.transform.localPosition = new Vector3(fPosX, fPosY);
+
+        return pObj;
+    }
+}
