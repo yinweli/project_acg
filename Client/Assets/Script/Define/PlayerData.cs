762475fabee81971db5bd8baa3be476addf68fee
yinweli <yinweli@yahoo.com.tw>
2015-04-04 17:40:58 +0800

    玩家資料補完

--- Client/Assets/Script/Define/PlayerData.cs
+++ Client/Assets/Script/Define/PlayerData.cs
@@ -5,7 +5,18 @@ using System.Collections.Generic;
 
 public class Looks
 {
-
+	/* [Save] */ public int iHair = 0; // 頭髮
+	/* [Save] */ public int iDecorative = 0; // 裝飾
+	/* [Save] */ public int iFace = 0; // 臉部
+	/* [Save] */ public int iEyes = 0; // 眼睛
+	/* [Save] */ public int iEyeback = 0; // 眼白
+	/* [Save] */ public int iEyebrow = 0; // 睫毛
+	/* [Save] */ public int iBlush = 0; // 腮紅
+	/* [Save] */ public int iMonth = 0; // 嘴巴
+	/* [Save] */ public int iBody = 0; // 身體
+	/* [Save] */ public int iFoot = 0; // 腳
+	/* [Save] */ public int iHand = 0; // 手
+	/* [    ] */ public int iEquip = 0; // 裝備
 }
 
 public class Member
