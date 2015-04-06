0e53d2ab9c6d4b56cdde7b5e60639d58d713fe30
水嵐 <joushior@yahoo.com.tw>
2015-04-06 15:13:37 +0800

    套用場景物件

--- Client/Assets/Script/System/MapMove.cs
+++ Client/Assets/Script/System/MapMove.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class MapMove : MonoBehaviour 
 {
+    public GameObject pCamCtrl;
+    public GameObject pObjBg;
 	public int RoadIndex = 0;
 	public Vector3 NextPos = new Vector3();
 
@@ -13,15 +15,10 @@ public class MapMove : MonoBehaviour
 		MapCreater.This.Create();
         NewGame();
 	}
-    // ------------------------------------------------------------------
-	void Update()
-    {/*
-        if (Vector2.Distance(Vector3.zero, MapCreater.This.GetRoadObj(NextRoad).transform.position) < 0.01f)
-            NextRoad++;
-
-        MoveTo(NextRoad);
-		MapCreater.This.Refresh(NextRoad);*/
-	}
+    void Update()
+    {
+        pObjBg.transform.position = pCamCtrl.transform.position;
+    }
     // ------------------------------------------------------------------
     void NewGame()
     {
@@ -29,13 +26,5 @@ public class MapMove : MonoBehaviour
         transform.localPosition = new Vector3(-MapCreater.This.GetRoadObj(0).transform.localPosition.x, -MapCreater.This.GetRoadObj(0).transform.localPosition.y, 0);
     }
     // ------------------------------------------------------------------
-    void MoveTo(int iRoad)
-    {
-        Vector3 vecDirection = MapCreater.This.GetRoadObj(iRoad - 1).transform.localPosition - MapCreater.This.GetRoadObj(iRoad).transform.localPosition;
-
-        // 把z歸零, 因為沒有要動z值.
-        vecDirection.z = 0;
-        // 把物件位置朝目標向量(玩家方向)移動.
-        transform.localPosition += vecDirection.normalized * GameDefine.fMoveSpeed * 2 * Time.deltaTime;
-    }
+    
 }
