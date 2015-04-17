b77654a636be07558714a9da50083ae8863d78f3
水嵐 <joushior@yahoo.com.tw>
2015-04-17 16:10:20 +0800

    機槍聲音檔修正

--- Client/Assets/Script/View/G_PLook.cs
+++ Client/Assets/Script/View/G_PLook.cs
@@ -3,15 +3,16 @@ using System.Collections;
 
 public class G_PLook : MonoBehaviour 
 {
+    public UI2DSprite[] Role;
     public void SetLook(AIPlayer pAI, int iLayer, WeaponType pWeapon)
     {
         GameObject ObjRHand = null;
-        UI2DSprite[] Role = GetComponentsInChildren<UI2DSprite>();
+        Role = GetComponentsInChildren<UI2DSprite>();
 
         // 取得右手.
         for (int i = 0; i < Role.Length; i++)
-            if (Role[i].gameObject.name == "Hand_R")
-                ObjRHand = Role[i].gameObject;
+            if (Role[i].gameObject.name == "S_Hand_R")
+                ObjRHand = Role[i].gameObject.transform.parent.gameObject;
 
         pAI.pAni = GetComponent<Animator>();
 
@@ -22,7 +23,13 @@ public class G_PLook : MonoBehaviour
             UI2DSprite[] p2DS = ObjWeapon.GetComponentsInChildren<UI2DSprite>();
             // 修改武器layer
             for (int i = 0; i < p2DS.Length; i++)
-                p2DS[i].depth = p2DS[i].depth + (iLayer * 15);
+            {
+                p2DS[i].depth = p2DS[i].depth + (iLayer * 20);
+
+                Vector3 vecPos = p2DS[i].gameObject.transform.localPosition;
+                vecPos.z = -0.00002f * (float)p2DS[i].depth;
+                p2DS[i].gameObject.transform.localPosition = vecPos;
+            }
 
             // 拿手電筒需替玩家加上光源.
             if (pWeapon == WeaponType.Weapon_001)
@@ -37,8 +44,14 @@ public class G_PLook : MonoBehaviour
 
         // 依照角色切換layer.
         for (int i = 0; i < Role.Length; i++)
-            Role[i].depth = Role[i].depth + (iLayer * 15);
+        {
+            Role[i].depth = Role[i].depth + (iLayer * 20);
 
-        Destroy(this);
+            Vector3 vecPos = Role[i].gameObject.transform.localPosition;
+            vecPos.z = -0.00002f * (float)Role[i].depth;
+            Role[i].gameObject.transform.localPosition = vecPos;
+        }
+
+        //Destroy(this);
     }
 }
