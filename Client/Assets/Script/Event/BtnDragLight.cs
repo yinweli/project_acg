c5d03d71da4e85f6da413a2b44f0b6bd9c83fcf1
水嵐 <joushior@yahoo.com.tw>
2015-04-18 17:20:28 +0800

    拯救角色途中

--- Client/Assets/Script/Event/BtnDragLight.cs
+++ Client/Assets/Script/Event/BtnDragLight.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class BtnDragLight : MonoBehaviour
 {
     public GameObject ObjRange;
+    public GameObject ObjRange_2;
 
     void OnDrag(Vector2 delta)
     {
         Vector3 diff = transform.position - ObjRange.transform.position;
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
         ObjRange.transform.rotation = Quaternion.Euler(0, 0, rot_z - 90);
+        ObjRange_2.transform.rotation = Quaternion.Euler(0, 0, rot_z - 90);
     }
 }
---
+++ Client/Assets/Script/Event/Btn_SaveRole.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Btn_SaveRole : MonoBehaviour 
+{
+    public AIPlayer pPlayer;
+    public Animator pAni;
+    // ------------------------------------------------------------------
+    void Start()
+    {
+        pPlayer = transform.parent.gameObject.GetComponent<AIPlayer>();
+    }
+    // ------------------------------------------------------------------
+    void OnClick()
+    {
+        PlayerCreater.pthis.SaveRole(pPlayer.gameObject);
+        Destroy(this);        
+    }
+    // ------------------------------------------------------------------
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Look")
+        {
+            pAni.Play("TalkShing");
+        }
+    }
+    // ------------------------------------------------------------------
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Look")
+        {
+            pAni.Play("Wait");
+        }
+    }
+	
+}
