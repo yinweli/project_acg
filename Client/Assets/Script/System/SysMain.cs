d7d3df1c619368a1947d802be7d1707a30afa5d2
水嵐 <joushior@yahoo.com.tw>
2015-04-18 14:19:02 +0800

    Revert "撿東西的prefab"
    This reverts commit ef35c9e61b204324c28e28cb4af3ca9b615b3216.

--- Client/Assets/Script/System/SysMain.cs
+++ Client/Assets/Script/System/SysMain.cs
@@ -54,7 +54,7 @@ public class SysMain : MonoBehaviour
 
             // 以下是測試資料, 以後要改
             Rule.MemberAdd(1);
-			Rule.MemberAdd(5);
+			Rule.MemberAdd(10);
 			GameData.pthis.iStyle = 1;
         }//if
         // 建立地圖.
@@ -171,8 +171,7 @@ public class SysMain : MonoBehaviour
     {
         bIsGaming = false;
         //Time.timeScale = 0;
-        GameObject pObj = SysUI.pthis.CreatePanel("Prefab/P_Victory");
-        pObj.transform.localPosition = new Vector3(0, 0, -1000);
+        SysUI.pthis.CreatePanel("Prefab/P_Victory");        
     }
     // ------------------------------------------------------------------
     void ClearObj()
