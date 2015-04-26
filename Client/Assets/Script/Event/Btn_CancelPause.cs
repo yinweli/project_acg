9efd94038dcec62039180c498deb49fdc33fe925
水嵐 <joushior@yahoo.com.tw>
2015-04-26 16:12:19 +0800

    暫停相關

---
+++ Client/Assets/Script/Event/Btn_CancelPause.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Btn_CancelPause : MonoBehaviour
+{
+    void OnClick()
+    {
+        Time.timeScale = 1;
+    }
+}
---
+++ Client/Assets/Script/Event/Btn_Music.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Btn_Music : MonoBehaviour 
+{
+    public UISprite pBg;
+	// Use this for initialization
+	void Start () 
+    {
+        if (AudioCtrl.pthis.pMusic.mute)
+            pBg.color = Color.gray;
+	}
+
+    void OnClick()
+    {
+        AudioCtrl.pthis.pMusic.mute = !AudioCtrl.pthis.pMusic.mute;
+
+        if (AudioCtrl.pthis.pMusic.mute)
+            pBg.color = Color.gray;
+        else
+            pBg.color = Color.white;
+    }
+}
