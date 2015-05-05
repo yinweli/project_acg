a2f2533f6f798beee655b98b2fabeaf0d4263fd1
水嵐 <joushior@yahoo.com.tw>
2015-05-05 17:12:04 +0800

    大絕按鈕實作

---
+++ Client/Assets/Script/Event/Btn_Bomb.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Btn_Bomb : MonoBehaviour 
+{
+    public UILabel pLbCount;
+    // 冷卻.
+    public float fCoolDown = 0;
+
+    void Update()
+    {
+        pLbCount.text = PlayerData.pthis.iBomb.ToString();
+    }
+
+    void OnClick()
+    {
+        if (fCoolDown > Time.time)
+            return;
+
+        GetComponent<UIButton>().isEnabled = false;
+
+        // 播放大絕.
+        SysBomb.pthis.StartBomb();
+
+        // 計算冷卻.
+        fCoolDown = Time.time + 5;
+
+        StartCoroutine(CoolDown());
+    }
+
+    IEnumerator CoolDown()
+    {
+        while (fCoolDown > Time.time)
+            yield return new WaitForEndOfFrame();
+
+        GetComponent<UIButton>().isEnabled = true;       
+    }
+}
---
+++ Client/Assets/Script/System/SysBomb.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SysBomb : MonoBehaviour 
+{
+    static public SysBomb pthis = null;
+    public Animator pAni;
+
+    void Awake()
+    {
+        pthis = this;
+    }
+
+    public void StartBomb()
+    {
+        // 播放動畫.
+        BombDamage();
+    }
+
+    public void BombDamage()
+    {
+        PlayerData.pthis.iBomb--;
+        foreach (GameObject ObjEnemy in SysMain.pthis.Enemy)
+        {
+            if (ObjEnemy && ObjEnemy.GetComponent<AIEnemy>())
+                ObjEnemy.GetComponent<AIEnemy>().AddHP(-GameDefine.iDamageBomb);
+        }
+        SysMain.pthis.SaveGame();
+    }
+}
