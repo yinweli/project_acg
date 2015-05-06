9713c8c59dc08bf68a25620e8564bcd82a6a4895
yinweli <yinweli@yahoo.com.tw>
2015-05-06 12:57:49 +0800

    新增怪物模式列舉

--- Client/Assets/Script/Define/EnumData.cs
+++ Client/Assets/Script/Define/EnumData.cs
@@ -77,6 +77,15 @@ public enum ENUM_ModeFeature
 	Frozen, // 冰凍
 }
 
+// 怪物模式列舉
+public enum ENUM_ModeMonster
+{
+	Null = 0, 
+	Normal, // 正常
+	ActiveLight, // 光照啟動
+	ActiveDark, // 黑暗啟動
+}
+
 // 射程列舉
 public enum ENUM_Range
 {
