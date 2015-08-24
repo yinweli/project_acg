d5b9efca4bd379912261bdc8161c2c644f85d870
水嵐 <joushior@yahoo.com.tw>
2015-08-24 18:26:27 +0800

    小書書製作途中

---
+++ Client/Assets/Script/View/G_Upgrade.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class G_Upgrade : MonoBehaviour 
+{
+    public ENUM_Weapon pWeapon = ENUM_Weapon.Null;
+
+    public UISprite pS_Icon;
+    public UILabel pLb_Lv;
+
+    public UILabel pLb_Ability;
+    public UISprite pS_AbilityBg;
+
+    public UISprite[] pS_Collection = new UISprite[5];
+    public UILabel[] pLb_Collection = new UILabel[5];
+
+    public UILabel pLb_Src;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+}
