9b24d4ee870e72e8271ec2e427f05e32ed1a3622
水嵐 <joushior@yahoo.com.tw>
2015-08-25 10:18:27 +0800

    成就的格子

---
+++ Client/Assets/Script/View/G_Achievement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class G_Achievement : MonoBehaviour 
+{
+    public ENUM_Achievement pAchieve = ENUM_Achievement.Null;
+
+    public UISprite pS_Icon;
+    public UISprite pS_Check;
+    public UILabel pLb_Reward;
+
+    public UILabel pLb_NowProgress;
+    public UILabel pLb_Lv;
+
+    public UILabel pLb_Src;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
