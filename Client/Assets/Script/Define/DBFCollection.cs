d0b21ec35ca5013109cfb1eaaca2bfc0d0d08a30
yinweli <yinweli@yahoo.com.tw>
2015-08-25 17:07:04 +0800

    先更新收集物品介面試試看

--- Client/Assets/Script/Define/DBFCollection.cs
+++ Client/Assets/Script/Define/DBFCollection.cs
@@ -6,7 +6,6 @@ using System.Linq;
 
 public class DBFCollection : DBF
 {
-	public int Name = 0; // 收集名稱
 	public int Lv1Reward = 0; // 等級1獎勵編號
 	public int Lv2Reward = 0; // 等級2獎勵編號
 	public int Lv3Reward = 0; // 等級3獎勵編號
--- Client/Assets/Script/View/G_Upgrade.cs
+++ Client/Assets/Script/View/G_Upgrade.cs
@@ -6,119 +6,115 @@ public class G_Upgrade : MonoBehaviour
     public ENUM_Weapon pWeapon = ENUM_Weapon.Null;
 
     public UISprite pS_Icon;
-    public UILabel pLb_Lv;
-
-    public UILabel pLb_Ability;
+	public UISprite pS_Check;
     public UISprite pS_AbilityBg;
-
     public UISprite[] pS_Collection = new UISprite[5];
     public UILabel[] pLb_Collection = new UILabel[5];
 
-    public UILabel pLb_Src;
+	public UILabel pLb_Name;
+	public UILabel pLb_Lv;
+    public UILabel pLb_Desc;
+	public UILabel pLb_EffectNow;
+	public UILabel pLb_EffectNext;
 
-	// Use this for initialization
 	void Start()
+	{
+		Refresh();
+	}
+	public void Refresh()
 	{
 		int iLevel = Mathf.Max(Mathf.Min(Rule.GetWeaponLevel(pWeapon), GameDefine.iMaxCollectionLv), 0);
 		int iLevelNext = iLevel + 1;
-		string szNote = GameDBF.pthis.GetLanguage(5000 + (int)pWeapon);
+		
+		if(iLevel < GameDefine.iMaxCollectionLv)
+		{
+			pS_Check.enabled = false;
+			pLb_Lv.text = iLevel.ToString();
+		}
+		else
+		{
+			pS_Check.enabled = true;
+			pLb_Lv.enabled = false;
+		}//if
+		
+		pLb_Name.text = GameDBF.pthis.GetLanguage(4000 + (int)pWeapon);
+		pLb_Desc.text = GameDBF.pthis.GetLanguage(5000 + (int)pWeapon);
+
 		string szHelp = GameDBF.pthis.GetLanguage(6000 + (int)pWeapon);
-		string szResult = "";
-
-		pLb_Lv.text = iLevel.ToString();
-		pLb_Ability.text = GameDBF.pthis.GetLanguage(4000 + (int)pWeapon);
-
+		
 		if(pWeapon == ENUM_Weapon.Light)
 		{
-			szResult = szNote;
-
 			if(iLevel > 0)
-				szResult += string.Format("\nEffect\n" + szHelp, Rule.UpgradeWeaponLight(iLevel).Item1);
-
+				pLb_EffectNow.text = string.Format("Effect\n" + szHelp, Rule.UpgradeWeaponLight(iLevel).Item1);
+			
 			if(iLevelNext <= GameDefine.iMaxCollectionLv)
-				szResult += string.Format("\nNext Level\n[b2b2b2ff]" + szHelp + "[-]", Rule.UpgradeWeaponLight(iLevelNext).Item1);
+				pLb_EffectNext.text = string.Format("Next Level\n" + szHelp, Rule.UpgradeWeaponLight(iLevelNext).Item1);
 		}//if
-
+		
 		if(pWeapon == ENUM_Weapon.Knife)
 		{
-			szResult = szNote;
-			
 			if(iLevel > 0)
-				szResult += string.Format("\nEffect\n" + szHelp, Rule.UpgradeWeaponKnife(iLevel));
+				pLb_EffectNow.text = string.Format("Effect\n" + szHelp, Rule.UpgradeWeaponKnife(iLevel));
 			
 			if(iLevelNext <= GameDefine.iMaxCollectionLv)
-				szResult += string.Format("\nNext Level\n[b2b2b2ff]" + szHelp + "[-]", Rule.UpgradeWeaponKnife(iLevelNext));
+				pLb_EffectNext.text = string.Format("Next Level\n" + szHelp, Rule.UpgradeWeaponKnife(iLevelNext));
 		}//if
-
+		
 		if(pWeapon == ENUM_Weapon.Pistol)
 		{
-			szResult = szNote;
-
 			int iValue = (int)((1.0f - Rule.UpgradeWeaponPistol(iLevel)) * 100.0f);
 			int iValueNext = (int)((1.0f - Rule.UpgradeWeaponPistol(iLevelNext)) * 100.0f);
 			
 			if(iLevel > 0)
-				szResult += string.Format("\nEffect\n" + szHelp, iValue);
+				pLb_EffectNow.text = string.Format("Effect\n" + szHelp, iValue);
 			
 			if(iLevelNext <= GameDefine.iMaxCollectionLv)
-				szResult += string.Format("\nNext Level\n[b2b2b2ff]" + szHelp + "[-]", iValueNext);
+				pLb_EffectNext.text = string.Format("Next Level\n" + szHelp, iValueNext);
 		}//if
-
+		
 		if(pWeapon == ENUM_Weapon.Revolver)
 		{
-			szResult = szNote;
-			
 			if(iLevel > 0)
-				szResult += string.Format("\nEffect\n" + szHelp, Rule.UpgradeWeaponRevolver(iLevel));
+				pLb_EffectNow.text = string.Format("Effect\n" + szHelp, Rule.UpgradeWeaponRevolver(iLevel));
 			
 			if(iLevelNext <= GameDefine.iMaxCollectionLv)
-				szResult += string.Format("\nNext Level\n[b2b2b2ff]" + szHelp + "[-]", Rule.UpgradeWeaponRevolver(iLevelNext));
+				pLb_EffectNext.text = string.Format("Next Level\n" + szHelp, Rule.UpgradeWeaponRevolver(iLevelNext));
 		}//if
-
+		
 		if(pWeapon == ENUM_Weapon.Eagle)
 		{
-			szResult = szNote;
-			
 			if(iLevel > 0)
-				szResult += string.Format("\nEffect\n" + szHelp, Rule.UpgradeWeaponEagle(iLevel));
+				pLb_EffectNow.text = string.Format("Effect\n" + szHelp, Rule.UpgradeWeaponEagle(iLevel));
 			
 			if(iLevelNext <= GameDefine.iMaxCollectionLv)
-				szResult += string.Format("\nNext Level\n[b2b2b2ff]" + szHelp + "[-]", Rule.UpgradeWeaponEagle(iLevelNext));
+				pLb_EffectNext.text = string.Format("Next Level\n" + szHelp, Rule.UpgradeWeaponEagle(iLevelNext));
 		}//if
-
+		
 		if(pWeapon == ENUM_Weapon.SUB)
 		{
-			szResult = szNote;
-			
 			if(iLevel > 0)
-				szResult += string.Format("\nEffect\n" + szHelp, Rule.UpgradeWeaponSUB(iLevel));
+				pLb_EffectNow.text = string.Format("Effect\n" + szHelp, Rule.UpgradeWeaponSUB(iLevel));
 			
 			if(iLevelNext <= GameDefine.iMaxCollectionLv)
-				szResult += string.Format("\nNext Level\n[b2b2b2ff]" + szHelp + "[-]", Rule.UpgradeWeaponSUB(iLevelNext));
+				pLb_EffectNext.text = string.Format("Next Level\n" + szHelp, Rule.UpgradeWeaponSUB(iLevelNext));
 		}//if
-
+		
 		if(pWeapon == ENUM_Weapon.Rifle)
 		{
-			szResult = szNote;
-			
 			if(iLevel > 0)
-				szResult += string.Format("\nEffect\n" + szHelp, Rule.UpgradeWeaponRifle(iLevel));
+				pLb_EffectNow.text = string.Format("Effect\n" + szHelp, Rule.UpgradeWeaponRifle(iLevel));
 			
 			if(iLevelNext <= GameDefine.iMaxCollectionLv)
-				szResult += string.Format("\nNext Level\n[b2b2b2ff]" + szHelp + "[-]", Rule.UpgradeWeaponRifle(iLevelNext));
+				pLb_EffectNext.text = string.Format("Next Level\n" + szHelp, Rule.UpgradeWeaponRifle(iLevelNext));
 		}//if
-
+		
 		if(pWeapon == ENUM_Weapon.LMG)
 		{
-			szResult = szNote;
-			
 			if(iLevel > 0)
-				szResult += string.Format("\nEffect\n" + szHelp, Rule.UpgradeWeaponLMG(iLevel));
+				pLb_EffectNow.text = string.Format("Effect\n" + szHelp, Rule.UpgradeWeaponLMG(iLevel));
 			
 			if(iLevelNext <= GameDefine.iMaxCollectionLv)
-				szResult += string.Format("\nNext Level\n[b2b2b2ff]" + szHelp + "[-]", Rule.UpgradeWeaponLMG(iLevelNext));
+				pLb_EffectNext.text = string.Format("Next Level\n" + szHelp, Rule.UpgradeWeaponLMG(iLevelNext));
 		}//if
-
-		pLb_Src.text = szResult;
 	}
 }
