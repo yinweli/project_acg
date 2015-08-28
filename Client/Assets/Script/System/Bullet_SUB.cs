4d099fd9c1b1d1e7e9bfa19db54caa50c389818b
水嵐 <joushior@yahoo.com.tw>
2015-08-28 11:41:35 +0800

    修正榴彈問題

--- Client/Assets/Script/System/Bullet_SUB.cs
+++ Client/Assets/Script/System/Bullet_SUB.cs
@@ -6,12 +6,11 @@ using System.Collections.Generic;
 public class Bullet_SUB : MonoBehaviour
 {
     public AIBullet pAI = null;
-    public Sprite pSprite = null;
     // ------------------------------------------------------------------
     void Start()
     {
-        if (Rule.GetWeaponLevel(ENUM_Weapon.SUB) > 0)
-            pAI.pRander.sprite = pSprite;
+        if (Rule.GetWeaponLevel(ENUM_Weapon.SUB) > 0 && GetComponent<Animator>())
+            GetComponent<Animator>().Play("NewBullet");
     }
     // ------------------------------------------------------------------
     void OnTriggerEnter2D(Collider2D other)
@@ -21,27 +20,26 @@ public class Bullet_SUB : MonoBehaviour
             Tuple<int, bool> Damage = Rule.BulletDamage(pAI.iPlayer, false);
 			AIEnemy pEnemy = other.gameObject.GetComponent<AIEnemy>();
 			
-			if(pEnemy)
-			{
-				pEnemy.AddHP(-Damage.Item1, Damage.Item2);
-				Statistics.pthis.RecordDamage(ENUM_Damage.SUB, Damage.Item1);
-			}//if
-
             if (Rule.GetWeaponLevel(ENUM_Weapon.SUB) > 0)
-				GroundHurt(other.gameObject, Rule.UpgradeWeaponSUB());
+                GroundHurt(other.gameObject.transform.position, Rule.UpgradeWeaponSUB());
+            else if (pEnemy)
+            {
+                pEnemy.AddHP(-Damage.Item1, Damage.Item2);
+                Statistics.pthis.RecordDamage(ENUM_Damage.SUB, Damage.Item1);
+            }
             else
                 Destroy(gameObject);
         }
     }
     // ------------------------------------------------------------------
     // 範圍傷害.
-    void GroundHurt(GameObject pObj, int iDamage)
+    void GroundHurt(Vector3 vPos, int iDamage)
     {
         if(GetComponent<BoxCollider2D>())
             GetComponent<BoxCollider2D>().enabled = false;
 
         // 沒有可作為目標的怪物.
-        if (!pObj || SysMain.pthis.AtkEnemy.Count <= 0)
+        if (SysMain.pthis.AtkEnemy.Count <= 0)
             return;
 
         pAI.bCanMove = false;
@@ -53,11 +51,14 @@ public class Bullet_SUB : MonoBehaviour
 
         foreach (KeyValuePair<GameObject, int> itor in SysMain.pthis.Enemy)
         {
-            float fDisObj = Vector2.Distance(pObj.transform.position, itor.Key.transform.position);
+            float fDisObj = Vector2.Distance(vPos, itor.Key.transform.position);
 
             // 比較距離.
             if (fDisObj < GameDefine.fSUBArea && itor.Key && itor.Key.GetComponent<AIEnemy>() && itor.Key.GetComponent<AIEnemy>().iHP > 0)
-                itor.Key.GetComponent<AIEnemy>().AddHP(-iDamage, false);            
+            {
+                itor.Key.GetComponent<AIEnemy>().AddHP(-iDamage, false);
+                Statistics.pthis.RecordDamage(ENUM_Damage.SUB, iDamage);
+            }
         }        
     }
     // ------------------------------------------------------------------
