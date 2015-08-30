0803e51703dd450d7805d3e40e45fb7b7a518970
水嵐 <joushior@yahoo.com.tw>
2015-08-30 18:19:40 +0800

    介面更動

---
+++ Client/Assets/Script/Event/Btn_ChrInfo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Btn_ChrInfo : MonoBehaviour
+{
+    public G_ListRole pData = null;
+
+    void OnPress(bool bIsDown)
+    {
+        if (bIsDown && pData.pInfo)
+        {
+            pData.pInfo.gameObject.SetActive(true);
+            pData.pInfo.SetInfo(pData.iPlayerID);
+        }
+        else if (!bIsDown && pData.pInfo)
+        {
+            pData.pInfo.Reset();
+            pData.pInfo.gameObject.SetActive(false);
+        }
+    }
+}
