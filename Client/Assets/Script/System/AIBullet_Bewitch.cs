77ee3e9f8480e094c75cec4482854805d3a0706c
水嵐 <joushior@yahoo.com.tw>
2015-09-22 20:41:55 +0800

    修改愛心

--- Client/Assets/Script/System/AIBullet_Bewitch.cs
+++ Client/Assets/Script/System/AIBullet_Bewitch.cs
@@ -15,10 +15,10 @@ public class AIBullet_Bewitch : MonoBehaviour
             Destroy(gameObject);
 
         // 追追追.
-        ToolKit.MoveTo(gameObject, ObjTarget.transform.position - transform.position, fSpeed);
+        ToolKit.MoveTo(gameObject, ObjTarget.transform.position - transform.position, fSpeed * 1.5f);
 
         // 距離夠近就魅惑.
-        if (Vector2.Distance(gameObject.transform.position, ObjTarget.transform.position) < 1.5f)
+        if (Vector2.Distance(gameObject.transform.position, ObjTarget.transform.position) < 0.015f)
         {
             ObjTarget.GetComponent<AIPlayer>().BeWitch(pMaster.gameObject, 1);
 
