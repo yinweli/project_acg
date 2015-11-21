290d0febdf5d049259e4acb8800b543d42d117f8
水嵐 <joushior@yahoo.com.tw>
2015-11-21 15:55:13 +0800

    修改武器升級提示畫面
    阿猴想像畫面!!煉成!!

--- Client/Assets/Script/View/P_WeaponLvUp.cs
+++ Client/Assets/Script/View/P_WeaponLvUp.cs
@@ -5,6 +5,7 @@ public class P_WeaponLvUp : MonoBehaviour
 {
     public ENUM_Weapon pWeapon = ENUM_Weapon.Null;
     public UISprite S_Weapon = null;
+    public UILabel Lb_Lv = null;
 
 	// Use this for initialization
 	void Start () 
@@ -12,5 +13,7 @@ public class P_WeaponLvUp : MonoBehaviour
         S_Weapon.spriteName = string.Format("ui_w{0:00}", (int)pWeapon);
         S_Weapon.MakePixelPerfect();
         S_Weapon.gameObject.transform.localScale = new Vector3(2, 2, 1);
-	}
+
+        Lb_Lv.text = string.Format("- [20ff00]{0}[-] -", Rule.GetWeaponLevel(pWeapon));
+    }
 }
